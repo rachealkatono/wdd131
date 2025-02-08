3a9070134b5772f78d8a33fbafcf2b5bcffe6ea8
rachealkatono <you@example.com>
2025-02-09 02:30:33 +0300

    gifted

--- scripts/review.js
+++ scripts/review.js
@@ -24,14 +24,8 @@ document.addEventListener('DOMContentLoaded', function() {
     
     document.getElementById('reviewCount').textContent = `Total reviews submitted: ${newCount}`;
 });
-// Update the current year dynamically in the footer
-const currentYearElement = document.getElementById("currentyear");
-if (currentYearElement) {
-    currentYearElement.textContent = new Date().getFullYear();
-}
+// Display current year dynamically
+document.getElementById("currentyear").textContent = new Date().getFullYear();
 
-// Update the "last modified" date dynamically in the footer
-const lastModifiedElement = document.getElementById("last-modified-date");
-if (lastModifiedElement) {
-    lastModifiedElement.textContent = document.lastModified;
-}
+// Display last modified date dynamically
+document.getElementById("last-modified-date").textContent = document.lastModified;
